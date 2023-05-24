cd1a2519d7d257822430aeb938d2422fe2a5af7b
Winnard Arthur <winnardarthur7@gmail.com>
2023-05-24 23:13:43 +0000

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Lora&family=Poppins&family=Roboto:ital,wght@0,100;0,400;0,500;1,700&display=swap');
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
